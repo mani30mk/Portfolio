4819fa88e21fa69ab7039af8212c9ebad8ff8536
mani30mk <maniofficial.ac.in@gmail.com>
2026-01-25 14:03:26 +0530

    logo fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,23 +11,23 @@ export const metadata: Metadata = {
   title: 'Manikandan | CS Engineer',
   description: 'BTech Computer Science (IOT) student specializing in backend systems, distributed architecture, and software development.',
   generator: 'v0.app',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+  //   icons: {
+  //     icon: [
+  //       {
+  //         url: '/icon-light-32x32.png',
+  //         media: '(prefers-color-scheme: light)',
+  //       },
+  //       {
+  //         url: '/icon-dark-32x32.png',
+  //         media: '(prefers-color-scheme: dark)',
+  //       },
+  //       {
+  //         url: '/icon.svg',
+  //         type: 'image/svg+xml',
+  //       },
+  //     ],
+  //     apple: '/apple-icon.png',
+  //   },
 }
 
 export default function RootLayout({
